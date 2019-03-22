47eb11734a1f4e52667c7141554efb95fc9af909
bweegee <bw33g33@gmail.com>
2019-03-22 11:46:47 -0600

    palindrome brain teaser added

---
+++ palindrome.rb
@@ -0,0 +1,25 @@
+# Write a function that takes in a string and returns true or false if the string is a palindrome.
+
+def palindrome
+  puts "Give me a word"
+  string = gets.strip
+  string_array = string.scan (/\w/)
+  last = string.length - 1
+  first = 0
+  palindrome = true
+  
+  string_array.each do |letter|
+    while last > first
+        if string_array[first] != string_array[last]
+          palindrome = false
+           break
+        else
+          last-=1
+          first+=1
+        end
+    end
+  end
+  puts "#{palindrome}"
+end
+
+palindrome()
